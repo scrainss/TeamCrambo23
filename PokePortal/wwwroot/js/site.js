4a70b68b46fb1544d179e3cbc9a1c03a261d5033
Jacob Klucher <klucher@etsu.edu>
2023-11-14 21:55:32 -0500

    Image modal added to home page

--- PokePortal/wwwroot/js/site.js
+++ PokePortal/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // Add this script to enable Bootstrap functionality
+    var myModal1 = new bootstrap.Modal(document.getElementById('myModal1'));
 
-// Write your JavaScript code.
+    // Uncomment the following line if you want to handle modal show event
+    // myModal1.show();
+
+    // Optional: If you want to handle modal dismissal
+    // myModal1.hide();
+
+    // Repeat the above lines for each modal
+});
