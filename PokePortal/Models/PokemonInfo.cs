e2309184500751241381dc3c540fda03035949aa
Jacob Klucher <klucher@etsu.edu>
2023-11-15 16:03:54 -0500

    Implemented PokeApi to grab sprites for pokemon based on species after inserting into storage.

---
+++ PokePortal/Models/PokemonInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace PokePortal.Models
+{
+    public class PokemonInfo
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ PokePortal/Models/PokemonSpriteResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PokePortal.Models
+{
+    public class PokemonSpriteResponse
+    {
+        public string Normal { get; set; }
+        public string Shiny { get; set; }
+    }
+}
